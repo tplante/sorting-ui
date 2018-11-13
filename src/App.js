b0c1c734354dfe9c7bf330fe4f6a70d9f128a369
Taylor Plante <taylorplante99@gmail.com>
2018-11-13 15:48:09 +0100

    build sorting ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+const options = [
+  { candidate: "Barack Obama", rank: 0 },
+  { candidate: "George Bush", rank: 0 },
+  { candidate: "Hillary Clinton", rank: 0 },
+  { candidate: "Bernie Sanders", rank: 0 },
+  { candidate: "Marco Rubio", rank: 0 },
+  { candidate: "Donald Trump", rank: 0 }
+];
+
+const containerStyles = {
+  width: "250px",
+  margin: "0 auto",
+  padding: "15px"
+};
+const buttonStyles = {
+  padding: "5px",
+  textAlign: "center",
+  backgroundColor: "#d8d8d8",
+  fontWeight: "bold",
+  cursor: "pointer"
+};
+const clearStyles = {
+  ...buttonStyles,
+  marginBottom: "15px"
+};
+const optionStyles = {
+  marginBottom: "5px",
+  cursor: "pointer"
+};
+const rankStyles = {
+  ...buttonStyles,
+  display: "inline-block",
+  width: "20px",
+  height: "20px",
+  marginRight: "5px"
+};
+const submitStyles = {
+  ...buttonStyles,
+  marginTop: "15px",
+  backgroundColor: "#00b06f",
+  color: "white"
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { options, totalRank: 0, submitted: false };
+  }
+
+  handleClear = () => {
+    const { options } = this.state;
+    options.forEach(option => (option.rank = 0));
+    this.setState({ options, totalRank: 0, submitted: false });
+  };
+
+  handleClick = optionIndex => {
+    let { options, totalRank } = this.state;
+    if (options[optionIndex].rank) {
+      totalRank--;
+      options.forEach(option => {
+        if (option.rank > options[optionIndex].rank) {
+          option.rank--;
+        }
+      });
+      options[optionIndex].rank = 0;
+    } else {
+      totalRank++;
+      options[optionIndex].rank += totalRank;
+    }
+    this.setState({ options, totalRank });
+  };
+
+  handleSubmit = () => {
+    this.setState({ submitted: !this.state.submitted });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div style={containerStyles}>
+        <div onClick={this.handleClear} style={clearStyles}>
+          Clear all
+        </div>
+        {this.state.options.map((option, i) => (
+          <div
+            key={`${option.candidate}-${i}`}
+            onClick={() => this.handleClick(i)}
+            style={optionStyles}
           >
-            Learn React
-          </a>
-        </header>
+            <div style={rankStyles}>
+              <div style={{ opacity: option.rank ? 1 : 0 }}>{option.rank}</div>
+            </div>
+            {option.candidate}
+          </div>
+        ))}
+        <div onClick={this.handleSubmit} style={submitStyles}>
+          {this.state.submitted ? "Nice!" : "Submit"}
+        </div>
       </div>
     );
   }
